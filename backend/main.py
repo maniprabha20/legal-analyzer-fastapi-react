b8f675e0fb715a189395a8acd6e0773ac7a906bc
maniprabha20 <maniprabhashankar@gmail.com>
2026-08-07 12:19:39 +0530

    Add CORS middleware for React frontend

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,20 @@ from fastapi import FastAPI
 
 from auth import router as auth_router
 from documents import router as documents_router 
-from reports import router as reports_router
+from reports import router as reports_router 
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="AI Legal Document Analyzer API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
